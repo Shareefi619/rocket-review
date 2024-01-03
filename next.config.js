f0abf7d358e5393dadf560d9b167039934776afe
Shareefi619 <irfanhaidershareefi6@gmail.com>
2024-01-04 02:37:31 +0500

    2nd commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,26 @@
 const nextConfig = {
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/,
-      use: {
-        loader: "file-loader",
-        options: {
-          publicPath: "/_next/static/media/",
-          name: "[name].[hash].[ext]",
+    config.module.rules.push(
+      {
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next/static/media/",
+            name: "[name].[hash].[ext]",
+          },
         },
       },
-    });
+      {
+        test: /\.svg$/,
+        issuer: {
+          test: /\.(js|ts)x?$/,
+          // for webpack 5 use
+          // { and: [/\.(js|ts)x?$/] }
+        },
+        use: ['@svgr/webpack'],
+      }
+    );
 
     return config;
   },
